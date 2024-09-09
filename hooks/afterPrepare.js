b37539b5438c3525e2f104f99f7d332712a50b74
mlerena <manuellerena@gmail.com>
2024-09-09 11:57:03 -0300

    Add ic_launcher png

--- hooks/afterPrepare.js
+++ hooks/afterPrepare.js
@@ -1,120 +1,138 @@
 #!/usr/bin/env node
 
-'use strict';
+'use strict'
 
-var fs = require('fs');
-var xml2js = require('xml2js');
-var path = require("path");
+var fs = require('fs')
+var xml2js = require('xml2js')
+var path = require('path')
 
 module.exports = function (context) {
-    if (context.opts.platforms.includes('ios')) {
-        return;
-    }
-    var projectRoot = context.opts.projectRoot;
-    var appSrcMainPath = path.join(projectRoot, 'platforms', 'android', 'app', 'src', 'main');
-    
-    var TV_ACTIVITY_NAME = 'com.kuack.plugins.androidtv.TvActivity';
-    // manifest
-    var manifestPath = path.join(appSrcMainPath, 'AndroidManifest.xml');
-    var androidManifest = fs.readFileSync(manifestPath).toString();
-    if (androidManifest) {
-        xml2js.parseString(androidManifest, function(err, manifest) {
-            if (err) return console.error(err);
-            
-            var manifestRoot = manifest['manifest'];
-            var applicationTag = manifestRoot.application[0]['$'];
-            applicationTag['android:banner'] = '@drawable/banner';
+  if (context.opts.platforms.includes('ios')) {
+    return
+  }
+  var projectRoot = context.opts.projectRoot
+  var appSrcMainPath = path.join(projectRoot, 'platforms', 'android', 'app', 'src', 'main')
 
-            var activityTags = manifestRoot.application[0].activity;
-            var tvActivityFound = false;
-            activityTags.some(function(activity, i){
-                if (activity['$']['android:name'] === TV_ACTIVITY_NAME) {
-                    tvActivityFound = true;
-                    return true;
-                }
-            });
-            if (!tvActivityFound) {
-                activityTags.push({
-                    '$': {
-                        'android:name': TV_ACTIVITY_NAME,
-                        'android:label': '@string/app_name',
-                        'android:theme': '@style/Theme.App.SplashScreen',
-                        'android:screenOrientation': 'landscape',
-                        'android:launchMode': 'singleInstance',
-                        'android:configChanges': 'keyboard|keyboardHidden|navigation',
-                        'android:exported': true,
-                    },
-                    'intent-filter': [{
-                        action: [{
-                            '$': {
-                                'android:name': 'android.intent.action.MAIN'
-                            }
-                        }],
-                        category: [{
-                            '$': {
-                                'android:name': 'android.intent.category.LEANBACK_LAUNCHER'
-                            }
-                        }],
-                    }]
-                });
+  var TV_ACTIVITY_NAME = 'com.kuack.plugins.androidtv.TvActivity'
+  // manifest
+  var manifestPath = path.join(appSrcMainPath, 'AndroidManifest.xml')
+  var androidManifest = fs.readFileSync(manifestPath).toString()
+  if (androidManifest) {
+    xml2js.parseString(androidManifest, function (err, manifest) {
+      if (err) return console.error(err)
+
+      var manifestRoot = manifest['manifest']
+      var applicationTag = manifestRoot.application[0]['$']
+      applicationTag['android:banner'] = '@drawable/banner'
+
+      var activityTags = manifestRoot.application[0].activity
+      var tvActivityFound = false
+      activityTags.some(function (activity, i) {
+        if (activity['$']['android:name'] === TV_ACTIVITY_NAME) {
+          tvActivityFound = true
+          return true
+        }
+      })
+      if (!tvActivityFound) {
+        activityTags.push({
+          '$': {
+            'android:name': TV_ACTIVITY_NAME,
+            'android:label': '@string/app_name',
+            'android:theme': '@style/Theme.App.SplashScreen',
+            'android:screenOrientation': 'landscape',
+            'android:launchMode': 'singleInstance',
+            'android:configChanges': 'keyboard|keyboardHidden|navigation',
+            'android:exported': true,
+          },
+          'intent-filter': [{
+            action: [{
+              '$': {
+                'android:name': 'android.intent.action.MAIN'
+              }
+            }],
+            category: [{
+              '$': {
+                'android:name': 'android.intent.category.LEANBACK_LAUNCHER'
+              }
+            }],
+          }]
+        })
+      }
+
+      var nonRequiredFeatures = [
+        //    'android.software.leanback',
+        'android.hardware.touchscreen',
+        'android.hardware.faketouch',
+        'android.hardware.telephony',
+        'android.hardware.camera',
+        'android.hardware.nfc',
+        'android.hardware.location.gps',
+        'android.hardware.microphone',
+        'android.hardware.sensor',
+        'android.hardware.screen.portrait',
+      ]
+      manifestRoot['uses-feature'] = manifestRoot['uses-feature'] || []
+      if (manifestRoot['uses-feature'].length > 0) {
+        manifestRoot['uses-feature'].forEach(function (manifestFeature) {
+          var currFeature = manifestFeature['$']
+          var currFeatureIndex = nonRequiredFeatures.indexOf(currFeature['android:name'])
+          if (currFeatureIndex > -1) {
+            nonRequiredFeatures.splice(currFeatureIndex, 1)
+            if (!currFeature['android:required'] || currFeature['android:required'] !== false) {
+              currFeature['android:required'] = false
             }
-
-            var nonRequiredFeatures = [
-            //    'android.software.leanback',
-                'android.hardware.touchscreen',
-                'android.hardware.faketouch',
-                'android.hardware.telephony',
-                'android.hardware.camera',
-                'android.hardware.nfc',
-                'android.hardware.location.gps',
-                'android.hardware.microphone',
-                'android.hardware.sensor',
-                'android.hardware.screen.portrait',
-            ];
-            manifestRoot['uses-feature'] = manifestRoot['uses-feature'] || [];
-            if (manifestRoot['uses-feature'].length > 0) {
-                manifestRoot['uses-feature'].forEach(function(manifestFeature){
-                    var currFeature = manifestFeature['$'];
-                    var currFeatureIndex = nonRequiredFeatures.indexOf(currFeature['android:name']);
-                    if (currFeatureIndex > -1) {
-                        nonRequiredFeatures.splice(currFeatureIndex, 1);
-                        if (!currFeature['android:required'] || currFeature['android:required'] !== false) {
-                            currFeature['android:required'] = false;
-                        }    
-                    }
-                });
+          }
+        })
+      }
+      manifestRoot['uses-feature'] = manifestRoot['uses-feature'].concat(
+        nonRequiredFeatures.map(function (featureName) {
+          return {
+            '$': {
+              'android:name': featureName,
+              'android:required': false
             }
-            manifestRoot['uses-feature'] = manifestRoot['uses-feature'].concat(
-                nonRequiredFeatures.map(function(featureName){
-                    return {
-                        '$': {
-                            'android:name': featureName,
-                            'android:required': false
-                        }
-                    };
-                })
-            );
+          }
+        })
+      )
 
-            var builder = new xml2js.Builder();
-            fs.writeFileSync(manifestPath, builder.buildObject(manifest), { encoding: 'utf8' });
-        });
+      var builder = new xml2js.Builder()
+      fs.writeFileSync(manifestPath, builder.buildObject(manifest), { encoding: 'utf8' })
+    })
+  }
+  let banners = [
+    {
+      src: path.join(projectRoot, 'res', 'icon', 'android', 'banner_160x90.png'),
+      dest: path.join(appSrcMainPath, 'res', 'drawable-mdpi', 'ic_banner.png'),
+      density: 'mdpi'
+    },
+    {
+      src: path.join(projectRoot, 'res', 'icon', 'android', 'banner_240x135.png'),
+      dest: path.join(appSrcMainPath, 'res', 'drawable-hdpi', 'ic_banner.png'),
+      density: 'hdpi'
+    },
+    {
+      src: path.join(projectRoot, 'res', 'icon', 'android', 'banner_320x180.png'),
+      dest: path.join(appSrcMainPath, 'res', 'drawable-xhdpi', 'ic_banner.png'),
+      density: 'xhdpi'
     }
-    // copy banner
-    var sourceFile = path.join(projectRoot, 'res', 'icon', 'android', 'banner.png');
-    var targetFile = path.join(appSrcMainPath, 'res', 'drawable-xhdpi', 'banner.png');
-    try {
-        fs.copyFileSync(sourceFile, targetFile);
-    } catch (err) {
-        console.log('cordova-plugin-androidtv: copy banner failed');
-        console.log(err);
-    }
-    // copy banner
-    var sourceFile = path.join(projectRoot, 'res', 'icon', 'android', 'ic_launcher.png');
-    var targetFile = path.join(appSrcMainPath, 'res', 'drawable-xhdpi', 'ic_launcher.png');
-    try {
-        fs.copyFileSync(sourceFile, targetFile);
-    } catch (err) {
-        console.log('cordova-plugin-androidtv: copy banner failed');
-        console.log(err);
-    }
-};
+  ]
+    banners.forEach(function (banner) {
+
+        try {
+            fs.copyFileSync(banner.src, banner.dest)
+        } catch (err) {
+            console.log('cordova-plugin-androidtv: copy banner failed')
+            console.log(err)
+        }
+    });
+
+  // copy icon
+  var sourceFile = path.join(projectRoot, 'res', 'icon', 'android', 'ic_launcher.png')
+  var targetFile = path.join(appSrcMainPath, 'res', 'drawable-xhdpi', 'ic_launcher.png')
+  try {
+    fs.copyFileSync(sourceFile, targetFile)
+  } catch (err) {
+    console.log('cordova-plugin-androidtv: copy banner failed')
+    console.log(err)
+  }
+}
